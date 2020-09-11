4be0f4dada6fabfb81b6d69ac9f51e680d1b35c6
Jose Villarroel <oscarclarke9900@gmail.com>
2020-09-11 17:31:43 +0200

    Sorter ready with number collection model

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi");
+import { Sorter } from "./Sorter";
+import { NumberCollection } from "./NumberCollection";
+
+const numberCollection = new NumberCollection([10, 2, -6, 0]);
+const sorter = new Sorter(numberCollection);
+sorter.sort();
+console.log(sorter.collection);
