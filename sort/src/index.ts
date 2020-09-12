0344fa7c4fcad32290c3edc5b6449db86bad0e89
Jose Villarroel <oscarclarke9900@gmail.com>
2020-09-12 23:56:26 +0200

    implemented characters collection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumberCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numberCollection = new NumberCollection([10, 2, -6, 0]);
-const sorter = new Sorter(numberCollection);
-sorter.sort();
-console.log(sorter.collection);
+const numberSorter = new Sorter(numberCollection);
+numberSorter.sort();
+console.log(numberSorter.collection);
+
+const characterCollection = new CharactersCollection("daswefcXDefa");
+const stringSorter = new Sorter(characterCollection);
+stringSorter.sort();
+console.log(stringSorter.collection);
