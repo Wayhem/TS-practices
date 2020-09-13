35110cca3dfc4f2c9ae2c7f7d00a10550e7b95e3
Jose Villarroel <oscarclarke9900@gmail.com>
2020-09-13 19:47:46 +0200

    abtracted sorter to be an abtract factory for the collection classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,31 @@
 import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumberCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
+// const numberCollection = new NumberCollection([10, 2, -6, 0]);
+// const numberSorter = new Sorter(numberCollection);
+// numberSorter.sort();
+// console.log(numberSorter.collection);
+
+// const characterCollection = new CharactersCollection("daswefcXDefa");
+// const stringSorter = new Sorter(characterCollection);
+// stringSorter.sort();
+// console.log(stringSorter.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(60);
+linkedList.add(1000);
+linkedList.add(-20);
+
+linkedList.sort();
+linkedList.print();
 
 const numberCollection = new NumberCollection([10, 2, -6, 0]);
-const numberSorter = new Sorter(numberCollection);
-numberSorter.sort();
-console.log(numberSorter.collection);
+numberCollection.sort();
+console.log(numberCollection.data);
 
 const characterCollection = new CharactersCollection("daswefcXDefa");
-const stringSorter = new Sorter(characterCollection);
-stringSorter.sort();
-console.log(stringSorter.collection);
+characterCollection.sort();
+console.log(characterCollection.data);
